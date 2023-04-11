84ab2f04aa6950600dd61c33ae6de79e2181b1aa
Mahmoud Marayef <mahmoudmarayef@gmail.com>
2023-04-11 23:19:49 +0200

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,15 @@
 import { Box } from "@chakra-ui/react";
 
 const Footer = () => (
-    <Box textAlign="center" padding="5" color="gray.600" borderTop="1px" borderColor="gray.100">
-        2022 Relator, Inc.
-    </Box>
-)
+  <Box
+    textAlign="center"
+    padding="5"
+    color="gray.600"
+    borderTop="1px"
+    borderColor="gray.100"
+  >
+    2023 Relator, Inc.
+  </Box>
+);
 
-export default Footer
+export default Footer;
